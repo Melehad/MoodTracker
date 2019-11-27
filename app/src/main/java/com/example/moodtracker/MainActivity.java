98f16603c04c90f2a0345105eb86437b785db9b9
lilidemon <adlibitum7@vivaldi.net>
2019-11-27 21:26:21 +0100

    creation of the first layout, activity-main, with the 3 basic elements, smiley, history and comments icons

--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.moodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImgHappy;
+    private ImageView mClickCom;
+    private ImageView mClickHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImgHappy = findViewById(R.id.smiley);
+        mClickCom = findViewById(R.id.com);
+        mClickHistory = findViewById(R.id.history);
     }
 }
