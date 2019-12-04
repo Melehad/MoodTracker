9cb84b8e4e6f84b9de8d18a31cdc6a0317bb3485
lilidemon <adlibitum7@vivaldi.net>
2019-12-04 21:59:30 +0100

    Setting up the Gesture Detector with movements onFling, installation of Share Preferencies with save and load methods, creation of custom_toast.xml

--- app/src/main/java/com/example/moodtracker/History.java
+++ app/src/main/java/com/example/moodtracker/History.java
@@ -2,15 +2,156 @@ package com.example.moodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import static com.example.moodtracker.MainActivity.STRCOLOR;
+import static com.example.moodtracker.MainActivity.SHARED_PREFS;
 
 public class History extends AppCompatActivity {
 
+    public static final String TEXT = "text";
+    private String text;
+    private Integer col;
+    private ImageView mClickComOne;
+    private ImageView mClickComTwo;
+    private ImageView mClickComThree;
+    private ImageView mClickComFour;
+    private ImageView mClickComFive;
+    private ImageView mClickComSix;
+    private ImageView mClickComSeven;
+
+    private int[] reLayTab = {R.id.rel_lay_today, R.id.rel_lay_yesterday, R.id.rel_lay_day_before_yesterday, R.id.rel_lay_three_days_ago
+            , R.id.rel_lay_four_days_ago, R.id.rel_lay_five_days_ago, R.id.rel_lay_six_days_ago, R.id.rel_lay_one_week_ago};
+
+    String textComTest;
+    String textComToday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+        mClickComOne = findViewById(R.id.btn_com_one);
+        mClickComTwo = findViewById(R.id.btn_com_two);
+        mClickComThree = findViewById(R.id.btn_com_three);
+        mClickComFour = findViewById(R.id.btn_com_four);
+        mClickComFive = findViewById(R.id.btn_com_five);
+        mClickComSix = findViewById(R.id.btn_com_six);
+        mClickComSeven = findViewById(R.id.btn_com_seven);
+
+        mClickComOne.setVisibility(View.INVISIBLE);
+        mClickComTwo.setVisibility(View.INVISIBLE);
+        mClickComThree.setVisibility(View.INVISIBLE);
+        mClickComFour.setVisibility(View.INVISIBLE);
+        mClickComFive.setVisibility(View.INVISIBLE);
+        mClickComSix.setVisibility(View.INVISIBLE);
+        mClickComSeven.setVisibility(View.INVISIBLE);
+
+        Resources res = getResources();
+        int[] myColorArray = res.getIntArray(R.array.colorArray);
+
+        RelativeLayout reLayToday = findViewById(R.id.rel_lay_today);
+        RelativeLayout reLayYesterday = findViewById(R.id.rel_lay_yesterday);
+        RelativeLayout reLayDayBeforeYesterday = findViewById(R.id.rel_lay_day_before_yesterday);
+        RelativeLayout reLayThreeDaysAgo = findViewById(R.id.rel_lay_three_days_ago);
+        RelativeLayout reLayFourDaysAgo = findViewById(R.id.rel_lay_four_days_ago);
+        RelativeLayout reLayFiveDaysAgo = findViewById(R.id.rel_lay_five_days_ago);
+        RelativeLayout reLaySixDaysAgo = findViewById(R.id.rel_lay_six_days_ago);
+        RelativeLayout reLayOneWeekAgo = findViewById(R.id.rel_lay_one_week_ago);
+
+        mClickComOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComTwo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComThree.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComFour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComFive.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComSix.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+        mClickComSeven.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                customToast(view);
+            }
+        });
+
     }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        col =  sharedPreferences.getInt(STRCOLOR, -1);
+    }
+
+    private void customToast(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.custom_toast_container));
+        //String strDate = getDate();
+        TextView textToast = layout.findViewById(R.id.text_toast);
+        //textToast.setText(strDate);
+        textToast.setText(textComToday);
+        Toast toast = new Toast(view.getContext());
+
+        toast.setGravity(Gravity.BOTTOM, 0, 20);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void customLayout() {
+
+        textComTest = text;
+        if(textComTest.equals("")) {
+            mClickComOne.setVisibility(View.INVISIBLE);
+            mClickComTwo.setVisibility(View.INVISIBLE);
+            mClickComThree.setVisibility(View.INVISIBLE);
+            mClickComFour.setVisibility(View.INVISIBLE);
+            mClickComFive.setVisibility(View.INVISIBLE);
+            mClickComSix.setVisibility(View.INVISIBLE);
+            mClickComSeven.setVisibility(View.INVISIBLE);
+        } else {
+            mClickComOne.setVisibility(View.VISIBLE);
+            mClickComTwo.setVisibility(View.VISIBLE);
+            mClickComThree.setVisibility(View.VISIBLE);
+            mClickComFour.setVisibility(View.VISIBLE);
+            mClickComFive.setVisibility(View.VISIBLE);
+            mClickComSix.setVisibility(View.VISIBLE);
+            mClickComSeven.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -3,27 +3,50 @@ package com.example.moodtracker;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import static com.example.moodtracker.R.id.edit_text_com;
+import static com.example.moodtracker.R.id.background_activity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView changeSmiley;
+    private static TextView mEdit;
+    private TextView mTextComSave;
 
     private ImageView mImgHappy;
     private ImageView mClickCom;
     private ImageView mClickHistory;
-    private static TextView mEdit;
 
     private String stringEditTextCom = "Vous n'avez encore rien écrit";
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String STRCOLOR = "color";
+
+    private GestureDetector mDetector;
+
+    private float down,up;
+    static final int MIN_DISTANCE = 150;
+    int position = 3;
+    private int[] tabSmiley = {R.drawable.smile_sad, R.drawable.smile_disappointed,
+            R.drawable.smile_normal, R.drawable.smile_happy, R.drawable.smile_sup_happy};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +56,19 @@ public class MainActivity extends AppCompatActivity {
         mImgHappy = findViewById(R.id.smiley);
         mClickCom = findViewById(R.id.com);
         mClickHistory = findViewById(R.id.history);
-        mEdit = findViewById(R.id.edit_text_com);
 
+        mEdit = findViewById(R.id.edit_text_com);
+        mTextComSave = findViewById(R.id.text_com_save);
+        changeSmiley = findViewById(R.id.smiley);
+
+        // this is the view we will add the gesture detector to
+        View myView = findViewById(background_activity);
+        // get the gesture detector
+        mDetector = new GestureDetector(this, new MyGestureListener());
+        // Add a touch listener to the view
+        // The touch listener passes all its events on to the gesture detector
+        myView.setOnTouchListener(touchListener);
+        // add com button listener
         // add com button listener
         mClickCom.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -53,10 +87,12 @@ public class MainActivity extends AppCompatActivity {
                                 stringEditTextCom = mEditTextCom.getText().toString();
 
                                 if(stringEditTextCom.equals("")){
+                                    saveData();
                                     Toast toast = Toast.makeText(view.getContext(),"Vous n'avez rien écrit", Toast.LENGTH_SHORT);
                                     toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM, 0, 30);
                                     toast.show();
                                 }else{
+                                    saveData();
                                     Toast toast = Toast.makeText(view.getContext(),"Commentaire enregistré", Toast.LENGTH_SHORT);
                                     toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM, 0, 30);
                                     toast.show();
@@ -88,4 +124,90 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, stringEditTextCom);
+        editor.apply();
+    }
+
+    public void saveColorData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(STRCOLOR, position);
+        editor.apply();
+    }
+
+
+    @SuppressLint("ResourceAsColor")
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return mDetector.onTouchEvent(event);
+        }
+    };
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            // don't return false here or else none of the other
+            // gestures will work
+            return true;}
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {return false;}
+        @Override
+        public void onLongPress(MotionEvent e) {}
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {return false;}
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {return false;}
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            up = event1.getY();
+            down = event2.getY();
+            float deltaY = up - down;
+
+
+            Resources res = getResources();
+            RelativeLayout backgroundActivity = findViewById(R.id.background_activity);
+            int[] myColorArray = res.getIntArray(R.array.colorArray);
+            // 0 red - 1 grey - 2 blue - 3 green - 4 yellow
+
+            if ((Math.abs(deltaY) > MIN_DISTANCE) && (down < up) && position > 0)
+            //slide vers le haut - montre les humeurs basses
+            {
+                position--;
+                backgroundActivity.setBackgroundColor(myColorArray[position]);
+                changeSmiley.setImageResource(tabSmiley[position]);
+            }
+
+            if ((Math.abs(deltaY) > MIN_DISTANCE) && (down > up) && position < 4)
+            //slide vers le bas - montre les humeurs hautes
+            {
+                position++;
+                backgroundActivity.setBackgroundColor(myColorArray[position]);
+                changeSmiley.setImageResource(tabSmiley[position]);
+            }
+
+            Log.i("TAG", "onFling: ");
+
+            return true;
+        }
+
+
+
+    }
+
+
 }
